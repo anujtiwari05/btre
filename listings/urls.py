20fd167ba0607d7743e1ab66aa8b06df467b6645
anujtiwari05 <anuj@babydestination.com>
2018-12-09 16:00:35 +0530

    added search functionality with one issue state is not filtering

--- listings/urls.py
+++ listings/urls.py
@@ -4,8 +4,8 @@ from . import views
 urlpatterns = [
 
     path('', views.index, name='listings'),
-    path('<int:listing_id>', views.listing, name='listing'),
-    path('search', views.search, name='search')
+    path('<int:listing_id>/', views.listing, name='listing'),
+    path('search/', views.search, name='search'),
 
 ]
 
